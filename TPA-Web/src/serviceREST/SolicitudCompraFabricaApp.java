f0ee0b5fc53eb02931b7ad2c2f2447fb45465a17
martinh <fmartinhuber@gmail.com>
2016-11-09 17:36:43 -0300

    - Cambios rest.

--- TPA-Web/src/serviceREST/SolicitudCompraFabricaApp.java
+++ TPA-Web/src/serviceREST/SolicitudCompraFabricaApp.java
@@ -1,22 +1,23 @@
 package serviceREST;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-
-import dto.SolicitudArticuloDTO;
-import serviceMessages.ProductorFabricaMessage;
+import javax.ws.rs.QueryParam;
 
 @Path("/service")
 public class SolicitudCompraFabricaApp {
 
 	@GET
 	@Path("/solicitudCompra")
+//	@Path("/{solicitudCompra}")
 	@Consumes({ "application/json" })
 	@Produces({ "application/json" })
-	public String solicitudCompra(String json) {
+	public String solicitudCompra(@DefaultValue("Unknown address") @QueryParam("solicitud") String json) {
 		
+		System.out.println(json.toString());
 //		SolicitudArticuloDTO solicitudArticulo = null; 
 //		ProductorFabricaMessage asd = new ProductorFabricaMessage();
 //		asd.sendMessage(solicitudArticulo);
