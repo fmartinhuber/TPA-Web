3ebd03e18171cc041e9657e5b3000bfe35a833d7
ramir <ramir@LAPTOP-3T82OQBM>
2016-11-10 20:15:22 -0300

    modf art

--- TPA-Web/src/servlets/CrearArticuloServlet.java
+++ TPA-Web/src/servlets/CrearArticuloServlet.java
@@ -82,79 +82,11 @@ public class CrearArticuloServlet extends HttpServlet {
 		
 		deposito.crearArticulo(newArticulo);
 		
-		response.getWriter().print("<h1> Se creó el artículo de tipo " +newArticulo.getTipo()+"<h1>");
-		
+		response.getWriter().print("<h1> Se creó el artículo de tipo " +newArticulo.getTipo()+"<h1>");		
 		response.getWriter().print("<p> <a href=\"/TPA-Web-0.0.1-SNAPSHOT/\">Regresar Menu</a></p>");		
-		
-		
-		
+					
 		// Enviarlo por REST
 		//CrearArticuloJAXRSClient.conexion(articuloDto);
-		
-		
-		
-		// Cantidad Disponible, Codigo, Descripcion, Fecha, Foto, Marca, Nombre, Origen, Precio 
-		// Electrodomestico: ficha tecnica
-		// Muebles: material
-		// Niños: edad 
-		// Moda: color y talle
-		
-//		ArticuloBean newArticulo = new ArticuloBean();
-//		
-//		Calendar cal01 = GregorianCalendar.getInstance(); 	
-//		cal01.add(Calendar.DAY_OF_YEAR, -1);	
-//		Date day01 = cal01.getTime();	
-//		cal01.add(Calendar.MONTH, 1); 		
-//		
-//		newArticulo.setId(ArticuloDao.getInstancia().obtenerMaximoIdArticulo());	
-//		newArticulo.setIdDeposito(request.getParameter("deposito"));
-//		newArticulo.setCodigo(Integer.valueOf(request.getParameter("codigo")));
-//		newArticulo.setDescripcion(request.getParameter("descripcion"));
-//		newArticulo.setFecha(day01);
-//		newArticulo.setFoto(request.getParameter("foto"));
-//		newArticulo.setMarca(request.getParameter("marca"));
-//		newArticulo.setNombre(request.getParameter("nombre"));
-//		newArticulo.setOrigen(request.getParameter("origen"));
-//		newArticulo.setPrecio(Float.valueOf(request.getParameter("precio")));		
-//		newArticulo.setCantidadDisponible(0);
-//		newArticulo.setTipo(request.getParameter("tipo"));
-//		
-//		newArticulo.setFichaTecnica(request.getParameter("fichaTecnica"));
-//		newArticulo.setMaterial(request.getParameter("material"));
-//		newArticulo.setEdadRecomendada(request.getParameter("edad"));
-//		newArticulo.setColor(request.getParameter("color"));
-//		newArticulo.setTalle(request.getParameter("talle"));
-//		
-//		System.out.println("codigo" +request.getParameter("codigo"));
-//		System.out.println("deposito" +request.getParameter("deposito"));
-//		System.out.println("descripcion" +request.getParameter("descripcion"));
-//		//System.out.println("fecha" +request.getParameter("fecha"));
-//		System.out.println("foto" +request.getParameter("foto"));
-//		System.out.println("marca" +request.getParameter("marca"));
-//		System.out.println("nombre" +request.getParameter("nombre"));
-//		System.out.println("origen" +request.getParameter("origen"));
-//		System.out.println("precio" +request.getParameter("precio"));
-//		//System.out.println("cantidad" +request.getParameter("cantidad"));
-//		System.out.println("tipo" +request.getParameter("tipo"));
-//		
-//		System.out.println("fichaTecnica" +request.getParameter("fichaTecnica"));
-//		System.out.println("material" +request.getParameter("material"));
-//		System.out.println("edad" +request.getParameter("edad"));
-//		System.out.println("color" +request.getParameter("color"));
-//		System.out.println("talle" +request.getParameter("talle"));
-//		
-//		try{		
-//			
-//			DepositoControlador.getInstancia().crearArticulo(newArticulo);
-//			response.getWriter().print("<h1> Se creó el artículo de tipo " +newArticulo.getTipo()+"<h1>");
-//			
-//			response.getWriter().print("<p> <a href=\"/tpia-rama/index.jsp\">Regresar Menu</a></p>");
-//			//<li class="active"><a href="index.jsp">MENU PRINCIPAL</a></li>
-//			
-//		}catch(Exception e){
-//			e.printStackTrace();
-//		}
-
 	}
 
 }
