bb06f81113de9745f80cafe5273b33dcdfccb4ae
Martin <fmartinhuber@gmail.com>
2016-11-11 00:25:16 -0300

    Mock + log4j + JMS

--- TPA-Web/src/servlets/PruebaServlet.java
+++ TPA-Web/src/servlets/PruebaServlet.java
@@ -58,6 +58,7 @@ public class PruebaServlet extends HttpServlet {
 		
 		SolicitudCompraDTO solicitudCompraDTO = new SolicitudCompraDTO();
 		solicitudCompraDTO.setCodigo(1);
+//		solicitudCompraDTO.setCodigo("1");
 		solicitudCompraDTO.setPendiente("pendiente");
 		solicitudCompraDTO.setSolicitudesArticulos(solicitudesArticulo);
 		ItemSolicitudCompraDTO itemSolicitudCompraDTO = new ItemSolicitudCompraDTO();
