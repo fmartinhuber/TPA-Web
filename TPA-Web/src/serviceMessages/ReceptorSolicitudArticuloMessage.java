4c6e6e8ac637f285516ab19ebb15ec16dbb7304b
Maximiliano Cao <Maximiliano Cao@192.168.0.29>
2016-11-12 18:15:03 -0300

    Updateando clases

--- TPA-Web/src/serviceMessages/ReceptorSolicitudArticuloMessage.java
+++ TPA-Web/src/serviceMessages/ReceptorSolicitudArticuloMessage.java
@@ -1,10 +1,14 @@
 package serviceMessages;
 
+import java.sql.Date;
+import java.util.Calendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.EJB;
 import javax.ejb.MessageDriven;
@@ -12,7 +16,11 @@ import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
+import org.apache.activemq.artemis.utils.json.JSONObject;
+
 import controler.IDepositoControladorLocal;
+import dto.ArticuloDTO;
+import dto.ItemSolicitudArticuloDTO;
 import dto.SolicitudArticuloDTO;
 
 /**
@@ -38,13 +46,23 @@ public class ReceptorSolicitudArticuloMessage implements MessageListener {
 	/**
      * @see MessageListener#onMessage(Message)
      */
-    public void onMessage(Message message) {
+    @SuppressWarnings("deprecation")
+	public void onMessage(Message message) {
     	try {
 			String messageText = null;
 			if(message instanceof TextMessage){
 				messageText = ((TextMessage)message).getText();
-				ObjectMapper mapper = new ObjectMapper();
-				SolicitudArticuloDTO solicitud = mapper.readValue(messageText, SolicitudArticuloDTO.class);
+				JSONObject json = new JSONObject();
+				
+				String despacho = json.getString("idDespacho");
+				ArticuloDTO articulo = deposito.obtenerArticuloPorCodigo(json.getString("codArticulo"));
+				SolicitudArticuloDTO solicitud = new SolicitudArticuloDTO();
+				solicitud.setCodigo(articulo.getCodArticulo());
+				solicitud.setEstado("Pendiente");
+				solicitud.setFechaEntrega(new Date(2016,12,31));
+				//solicitud.setIdDespacho(json.getString("idDespacho"));
+				ItemSolicitudArticuloDTO itemSolArt = new ItemSolicitudArticuloDTO(articulo,Integer.parseInt(json.getString("cantidad")));
+								  											
 				deposito.crearSolicitudArticulo(solicitud);
 			}
 			Logger.getAnonymousLogger().info("Mensaje recibido: " + messageText);
